e82d8fd3f94bea525d5e7b021230dfc41c0a3ff0
shambu2k <sidharthshambu00@gmail.com>
2019-05-28 00:23:01 +0530

    Moves up and Down perfectly but.. image goes below nav bar Goes to portrait Y in landscape mode

--- app/src/main/java/com/shambu/reversegravitythanos/MainActivity.java
+++ app/src/main/java/com/shambu/reversegravitythanos/MainActivity.java
@@ -1,52 +1,54 @@
 package com.shambu.reversegravitythanos;
 
+import android.animation.ObjectAnimator;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     int c=0;
     private ImageView vstone;
-    private Animation transYd;
-    private Animation transYu;
-    private TextView coors;
+    private ObjectAnimator objani;
+    private LinearLayout lila;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         vstone=(ImageView)findViewById(R.id.powerStone);
-        coors = (TextView)findViewById(R.id.textView);
-
+        lila = (LinearLayout)findViewById(R.id.mainlayout);
     }
+
+
 
     public void ontouch(View v){
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-        coors.setText("X is: "+width+"\nY is: "+height);
-        if (c%2==0) {
-            transYd = new TranslateAnimation(0, 0, 0, height);
-            transYd.setDuration(2000);
-            transYd.setFillAfter(true);
-            vstone.startAnimation(transYd);
+            Display display = getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            display.getSize(size);
+            float width = (float) size.x;
+            float height = (float) size.y;
+            if (c % 2 == 0) {
+                objani = ObjectAnimator.ofFloat(vstone, "y", height);
+                objani.setDuration(2000);
+                objani.start();
+            }
+            if (c % 2 == 1) {
+                objani = ObjectAnimator.ofFloat(vstone, "y", 0);
+                objani.setDuration(2000);
+                objani.start();
+            }
+            c++;
         }
-        if (c % 2==1){
-            transYu = new TranslateAnimation(0, 0, height, 0);
-            transYu.setDuration(2000);
-            transYu.setFillAfter(true);
-            vstone.startAnimation(transYu);
-        }
-        c++;
-    }
+
+
 
 }
+
+
+
+
