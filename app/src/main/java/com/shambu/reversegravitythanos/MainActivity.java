69d7bea10b4c979017226258bd54452ee46d4e84
shambu2k <sidharthshambu00@gmail.com>
2019-05-28 12:34:58 +0530

    A few but unsatisfactory fixes Fixed landscape mode image going to portrait Y, but still goes out of view. Changed image. Added other stones for hackermode. Hackermode yet to be implemented

--- app/src/main/java/com/shambu/reversegravitythanos/MainActivity.java
+++ app/src/main/java/com/shambu/reversegravitythanos/MainActivity.java
@@ -1,37 +1,39 @@
 package com.shambu.reversegravitythanos;
 
 import android.animation.ObjectAnimator;
+import android.content.res.Configuration;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     int c=0;
     private ImageView vstone;
     private ObjectAnimator objani;
-    private LinearLayout lila;
+    private TextView coors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         vstone=(ImageView)findViewById(R.id.powerStone);
-        lila = (LinearLayout)findViewById(R.id.mainlayout);
+        coors = (TextView)findViewById(R.id.textView);
     }
 
 
 
     public void ontouch(View v){
-            Display display = getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            float width = (float) size.x;
-            float height = (float) size.y;
+        int orientation = getResources().getConfiguration().orientation;
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        float height = (float) size.y;
+        if(orientation== Configuration.ORIENTATION_PORTRAIT) {
             if (c % 2 == 0) {
                 objani = ObjectAnimator.ofFloat(vstone, "y", height);
                 objani.setDuration(2000);
@@ -43,6 +45,24 @@ public class MainActivity extends AppCompatActivity {
                 objani.start();
             }
             c++;
+            coors.setText("Y: "+height);
+        }
+        else {
+            if (c % 2 == 0) {
+                objani = ObjectAnimator.ofFloat(vstone, "y", height);
+                objani.setDuration(2000);
+                objani.start();
+            }
+            if (c % 2 == 1) {
+                objani = ObjectAnimator.ofFloat(vstone, "y", 0);
+                objani.setDuration(2000);
+                objani.start();
+            }
+            c++;
+            coors.setText("Y: "+height);
+        }
+
+
         }
 
 
