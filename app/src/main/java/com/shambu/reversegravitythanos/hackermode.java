9f35fa53930516112c5e5c4662ae9b256c7b0027
shambu2k <sidharthshambu00@gmail.com>
2019-05-31 10:01:11 +0530

    Enabled Fullscreen and.. added new app icon

--- app/src/main/java/com/shambu/reversegravitythanos/hackermode.java
+++ app/src/main/java/com/shambu/reversegravitythanos/hackermode.java
@@ -30,6 +30,23 @@ public class hackermode extends AppCompatActivity {
         img_5=(ImageView)findViewById(R.id.imageView5);
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if(hasFocus){
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+
+
+
     public void rndloc(View v2){
 
         display = getWindowManager().getDefaultDisplay();
